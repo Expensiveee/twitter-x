9db3ac3d6fb3e0adb3fa9c42687e643381e52df4
Expensiveee <TheExpensiveee@gmail.com>
2023-03-25 16:13:15 +0000

    Update Spinner.jsx

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -1,6 +1,6 @@
 export default function () {
   return (
-    <div className="w-full h-40 flex items-start justify-center ">
+    <div className="w-full h-20 flex items-start justify-center ">
       <span className="w-14 h-14 border-t-transparent animate-spin m-auto text-neutral-200 border border-twitter-300 rounded-full"></span>
     </div>
   );
