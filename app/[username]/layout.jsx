34fe3a8f2e0bd61446c69d310f9ac6a50bc1be61
Expensiveee <TheExpensiveee@gmail.com>
2023-03-26 14:45:36 +0000

    Update layout.jsx

--- app/[username]/layout.jsx
+++ app/[username]/layout.jsx
@@ -2,14 +2,18 @@ import WhoToFollow from "@components/widgets/WhoToFollow";
 import axios from "axios";
 
 export async function generateMetadata({ params }) {
-  const res = await axios.get(
-    `https://twitter-x.expensiveee.me/api/user/${params.username}`
-  );
+  try {
+    await axios.get(
+      `https://twitter-x.expensiveee.me/api/user/${params.username}`
+    );
 
-  if (!res.ok)
-    return { title: "User Not Found", description: "T'as déjà K avec un 12 ?" };
-
-  return { title: res.data.username, description: res.data.bio };
+    return { title: res.data.username, description: res.data.bio };
+  } catch (error) {
+    return {
+      title: "User Not Found",
+      description: "T'as déjà K avec un 12 ?",
+    };
+  }
 }
 
 export default function Layout({ children }) {
